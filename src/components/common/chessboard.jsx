e36a18b8c39bb41e53f072ec0f99297018445a21
utility4u <benedikthofirek@gmail.com>
2019-11-18 13:19:25 +0100

    second commit

--- src/components/common/chessboard.jsx
+++ src/components/common/chessboard.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import startingPosition from "../others/startingPosition";
+import { boardCoordinates } from "../others/boardCoordinates";
 
-const Chessboard = () => {
-  return(<div></div>);
+const Chessboard = ({ position, playerColour }) => {
+  const boardCoordinatesByPlayerSide =
+    playerColour === "white" ? boardCoordinates : boardCoordinates.reverse();
+  console.log(boardCoordinatesByPlayerSide);
+  return <div></div>;
 };
 
 export default Chessboard;
--- src/components/common/game.jsx
+++ src/components/common/game.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import Notation from "./notation";
 import Chessboard from "./chessboard";
+import startingPosition from "../others/startingPosition";
+import { whileStatement } from "@babel/types";
 
 export default class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      position: startingPosition,
+      playerColour: "white",
+      gameRecord: []
+    };
+  }
   render() {
+    const { position, gameRecord, playerColour } = this.state;
     return (
       <div>
-        <Chessboard />
-        <Notation />
+        <Chessboard 
+        position={position} 
+        playerColour={playerColour}
+        />
+        <Notation gameRecord={gameRecord} />
       </div>
     );
   }
