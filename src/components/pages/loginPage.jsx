73d3f9a57c09dce6cb85454e64d55f21ad025aa9
utility4u <benedikthofirek@gmail.com>
2019-11-22 18:35:44 +0100

    18:35:43

--- src/components/pages/loginPage.jsx
+++ src/components/pages/loginPage.jsx
@@ -3,10 +3,44 @@ import Header from "../common/header";
 import { Link } from "react-router-dom";
 
 class LoginPage extends Component {
+  
+  
+  handleChange(event) {
+    const name = event.name;
+    const value = event.target.value;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
         <Header />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Username:
+            <input
+              name="username"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <h3>Not registered yet?</h3>
         <Link to="/register">Register</Link>
       </div>
     );
