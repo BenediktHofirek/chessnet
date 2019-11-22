1c54965710e56220e2da9c498704ba6844a23828
utility4u <benedikthofirek@gmail.com>
2019-11-22 14:45:01 +0100

    14:45:01

--- src/components/pages/loginPage.jsx
+++ src/components/pages/loginPage.jsx
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 import Header from "../common/header";
 
 class LoginPage extends Component {
-  state = {};
+  constructor(){
+    super();
+    this.state = {
+      login: true
+    }
+  }
+
+  handleSwitch(tab){
+      this.setState({switch: tab})
+  }
   render() {
+    const {login} = this.state;
     return (
       <div>
         <Header />
-        LoginPage ...
+        {login ?}
       </div>
     );
   }
