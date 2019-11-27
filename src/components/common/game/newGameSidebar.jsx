e3c3572b989df0651a11f95b6ed65ee94f6d31a9
utility4u <benedikthofirek@gmail.com>
2019-11-27 11:15:14 +0100

    11:15:14

--- src/components/common/game/newGameSidebar.jsx
+++ src/components/common/game/newGameSidebar.jsx
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 class NewGameSidebar extends Component {
     state = {  }
     render() { 
-        return ( <div className="newGameSidebar"></div> );
+        const {timeControl, preferedTempo} = this.state;
+        const {handleNewGame} = this.props;
+        return ( <div className="newGameSidebar">
+            <button onClick={handleNewGame(timeControl, preferedTempo)}>New Game</button>
+            <form>
+            <label>
+            Select tempo:
+            <select multiple={true} value={timeControl} onChange={this.handleTimeControlChange}>
+                <option value="grapefruit">Grapefruit</option>
+                <option value="lime">Lime</option>
+                <option value="coconut">Coconut</option>
+                <option value="mango">Mango</option>
+            </select>
+            </label>
+            </form>
+        </div> );
     }
 }
  
